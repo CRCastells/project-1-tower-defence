f7af72ddd3bfd80549f994c8082030dc2bb7391b
Kelrynn <cameroncastells@gmail.com>
2017-09-27 15:36:41 -0600

    refactoring code

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,37 +1,28 @@
-
 //instance variables for stats/traits of sprites
 //user data
-var waveTime = 10;
+var waveTime = 30;
 var gold = 500;
 var cost = 300;
 var level = 0;
+var counter = 0;
 var lives = 30;
 //sprite data
 var enemies = [];
-var towers= [];
-var img;
-//enemy sprite group
-var eSprites;
-//bullet group
-var bullets;
-//walls
-var bottom;
-var exit;
-// image data
-var animated
-var tImage;
+var towers = [];
+//enemy sprite/bullets group;walls;image data; xy coords;
+var eSprites,bullets,bottom,exit,img,animated,tImage,eneX,eneY,bull;
 
-
+/* -------------------- canvas functions  -------------------- */
 //images to grab prior to the page loading
 function preload() {
  animated = loadAnimation("/images/run/24.png","/images/run/31.png");
  img = loadImage("../images/playarea.png");
  tImage = loadImage("/images/tower.png");
+ bull = loadImage("/images/bullet.png");
 }
 //one time run of the canvas/ functions
-//functions defined in p5 or p5.play must be used in setup or draw
+//functions defined in p5 or p5.play must be used in setup or draw or in functions only used within the two
 function setup() {
-
   //create the canvas 900x550
   var canv = createCanvas(900,550);
   //link the canvas to the div #canvasholder
@@ -39,8 +30,6 @@ function setup() {
   //create a sprite along the bottom and right of the canvas to act as a wall
   bottom = createSprite(500,550,600,0);
   exit = createSprite(900,250,0,300);
-
-
   //make it rigid
   bottom.immovable = true;
   exit.immovable = true;
@@ -48,134 +37,12 @@ function setup() {
   eSprites = new Group();
   //define bulelts group for collisions
   bullets = new Group();
-  
-
-
-	//function to place all enemy sprites on canvas
-	function wave(level){
-		//loop 10 times to place sprites
-		for (let i = 0; i < 10 +(level * 2); i++) {
-			//wait 300ms
-			setTimeout(function(){
-				//new enemy
-				let sp = new Enemy();
-				//add that sprite to the enemy sprites group
-				sp.sprite.addToGroup(eSprites);
-				//shoot the sprite onto the canvas and downwards toward the 'exit'
-				sp.sprite.setSpeed(sp.speed,sp.direction);
-				//just a backup to delete the sprite if it doenst get removed by colliders
-				sp.sprite.life = 385;
-				//add the enemy object into the enemies array
-				enemies.push(sp);
-			},i*300);
-		} 	
-
-	}
-	//wave countdown timer
-	function timer(){
-		//every second
-		setInterval(function(){
-			//deduct the wavetime
-				waveTime--;
-			// if the wave is out of time, reset the timer and stop the function
-			if (waveTime <= 0){
-				//reset the wave time
-				waveTime = 10;
-				return;
-			}
-		},1000);
-	}
-
-	// begins the wave by calling the wave and timer functions and incrementing the level for next call
-	// on repeat every 10 seconds.
-	function startWaves(){
-		setInterval(function(){
-			if(lives <= 0) return;
-			wave(level);
-			
-			level++;
-		},10000);	
-	}
 	//first time calls to set up the waves
 	timer();
 	startWaves();
-
-}
-function mouseClicked() {
-	//can player afford the cost of the tower?
-	if (gold > cost){
-		//place a tower at the clicked position and take away the gold cost
-		var tower = new Tower(mouseX,mouseY);
-		gold = gold - cost;
-		cost += 15;
-	}
-}
-//tower constructor
-function Tower(x, y) {
-	//range stat, how many pixels away can it fire?
-		this.range = 150;
-		//how much dmg the tower will do to the enemy's hp
-		this.dmg = 5;
-		//how fast it can fire, in ms
-		this.fireRate = 500;
-		//tower coords
-		this.x = x;
-		this.y = y;
-		//actual sprite on the canvas
-		this.sprite = createSprite(x,y,75,75);
-		//image for the sprite
-		this.sprite.addImage(tImage);
-		//add to tower array
-		towers.push(this);
 }
 
-//enemey constructor for the waves of enemies
-function Enemy(){
-		//the actual sprite on screen
-		this.sprite = createSprite(10,0,50,50);
-		//angle of entry to the canvas
-		this.direction = 41;
-		//hp stat, how many bullets can it take before removing from canvas
-		this.hp = 10;
-		//how fast the sprite moves
-		this.speed = 4;
-		//what the sprite looks like
-		this.sprite.addAnimation("running",animated);
-
-}
-
-// 	a tower will fire every 500ms
-// setinterval(fire(),500);
-
-function fire(towerPosX, towerPosY, range = 10){
-
-
-// fire will create a bullet
-// bullet = createSprite()
-	var projectile = createSprite(towerPosX,towerPosY, 10,10);
-// bullet will be sent toward [attractionPoint()] the enemy
-// bullet.attractionPoint(30,eSprites[0].enemyXpos,eSprites[0].enemyYpos);
-	projectile.addToGroup(bullets);
-	//remove bullets after the range is hit.
-	projectile.life = range;
-	projectile.attractionPoint(40,eneX,eneY);
-// the location of the first enemy will be used.
-}
-
-
-
-function youLose(){
-	//grab the box
-	var box = document.querySelector('.over');
-	//make the game over screen display
-	box.style.display = 'inherit';
-	//delete the canvas
-	remove();
-}
-var counter = 0;
-var eneX, eneY;
 function draw() {
-
 	//add background to the canvas`
 	background(img);
 	//required to have any sprites appear
@@ -185,8 +52,7 @@ function draw() {
   	document.getElementById('wave').innerText = "Wave Time: " + waveTime;
   	document.getElementById('level').innerText = "Level: " + level;
   	document.getElementById('gold').innerText = "Gold: " + gold;
-
-  	
+  	document.getElementById('description').innerText = "Number of Enemies in next Level: " + (10+(level*level));
   	//when enemy sprites hit the bottom, bounce!
   	eSprites.bounce(bottom,function(sprite){
   		//bounce does all the position work for me, 
@@ -204,7 +70,6 @@ function draw() {
   		enemies.shift();
 
   	})
-
   	//when a bullet hits an enemy, remove both and give the player 5 gold.
   	eSprites.collide(bullets,function(sprite1,sprite2){
   		sprite1.remove();
@@ -213,7 +78,6 @@ function draw() {
   		gold +=5;
 
   	});
-
   	//has the player lost?
 	if (lives <= 0) {
 		//stops draw loop
@@ -221,9 +85,8 @@ function draw() {
 		//ends the game
 		youLose();
 	}
-
 	//fire rate of the towers. dont want them to rapid fire now do we?
-	if(counter > 30){
+	if(counter > 10){
 
 		counter = 0;
 		towers.forEach(function(tower){
@@ -232,20 +95,116 @@ function draw() {
 		})
 	}
 	counter++;
-
-
 }
 
+function mouseClicked() {
+	//can player afford the cost of the tower?
+	if (gold >= cost){
+		//place a tower at the clicked position and take away the gold cost
+		var tower = new Tower(mouseX,mouseY);
+		gold = gold - cost;
+		cost += 15;
+	}
+}
+/* -------------------- self defined functions -------------------- */
+//enemey constructor for the waves of enemies
+function Enemy(){
+	//the actual sprite on screen
+	this.sprite = createSprite(10,0,50,50);
+	//angle of entry to the canvas
+	this.direction = 41;
+	//hp stat, how many bullets can it take before removing from canvas
+	this.hp = 10;
+	//how fast the sprite moves
+	this.speed = 4;
+	//what the sprite looks like
+	this.sprite.addAnimation("running",animated);
+}
 
+//tower constructor
+function Tower(x, y) {
+	//range stat, how many pixels away can it fire?
+	this.range = 150;
+	//how much dmg the tower will do to the enemy's hp
+	this.dmg = 5;
+	//how fast it can fire, in ms
+	this.fireRate = 500;
+	//tower coords
+	this.x = x;
+	this.y = y;
+	//actual sprite on the canvas
+	this.sprite = createSprite(x,y,75,75);
+	//image for the sprite
+	this.sprite.addImage(tImage);
+	//add to tower array
+	towers.push(this);
+}
 
+function fire(towerPosX, towerPosY, range = 20){
+	// 	a tower will fire every 500ms
+	// setinterval(fire(),500);
+	// fire will create a bullet
+	// bullet = createSprite()
+	var projectile = createSprite(towerPosX,towerPosY, 10,10);
+	// bullet will be sent toward [attractionPoint()] the enemy
+	// bullet.attractionPoint(30,eSprites[0].enemyXpos,eSprites[0].enemyYpos);
+	projectile.addToGroup(bullets);
+	projectile.addImage(bull);
+		//remove bullets after the range is hit.
+	projectile.life = range;
+	projectile.attractionPoint(30,eneX,eneY);
+	// the location of the first enemy will be used.
+}
 
+//wave countdown timer
+function timer(){
+	//every second
+	setInterval(function(){
+		//deduct the wavetime
+			waveTime--;
+		// if the wave is out of time, reset the timer and stop the function
+		if (waveTime <= 0){
+			//reset the wave time
+			waveTime = 30;
+			return;
+		}
+	},1000);
+}
 
+function startWaves(){
+	setInterval(function(){
+		if(lives <= 0) return;
+		wave(level);
+		
+		level++;
+	},30000);	
+}
 
+//function to place all enemy sprites on canvas
+function wave(level){
+	//loop 10 times to place sprites
+	for (let i = 0; i < 10 +(level * level); i++) {
+		//wait 300ms
+		setTimeout(function(){
+			//new enemy
+			let sp = new Enemy();
+			//add that sprite to the enemy sprites group
+			sp.sprite.addToGroup(eSprites);
+			//shoot the sprite onto the canvas and downwards toward the 'exit'
+			sp.sprite.setSpeed(sp.speed,sp.direction);
+			//just a backup to delete the sprite if it doenst get removed by colliders
+			sp.sprite.life = 385;
+			//add the enemy object into the enemies array
+			enemies.push(sp);
+		},i*200);
+	} 	
+}
 
-
-
-
-
-
-
-
+function youLose(){
+	//grab the box
+	var box = document.querySelector('.over');
+	//make the game over screen display
+	box.style.display = 'inherit';
+	//delete the canvas
+	remove();
+}
