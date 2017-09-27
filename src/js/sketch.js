124032e2f3c52410a65383ed2dd5c72e2d05e796
Kelrynn <cameroncastells@gmail.com>
2017-09-27 12:36:42 -0600

    bullet logic

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,12 +1,13 @@
 
 //instance variables for stats/traits of sprites
 //user data
-var waveTime = 30;
+var waveTime = 10;
 var gold = 500;
 var level = 0;
 var lives = 30;
 //sprite data
 var enemies = [];
+var towers= [];
 var img;
 //enemy sprite group
 var eSprites;
@@ -16,6 +17,8 @@ var exit;
 var animated
 var tImage;
 var playerLives;
+
+var bullets;
 
 //images to grab prior to the page loading
 function preload() {
@@ -33,7 +36,7 @@ function setup() {
   canv.parent('canvasHolder');
   //create a sprite along the bottom and right of the canvas to act as a wall
   bottom = createSprite(500,550,600,0);
-  exit = createSprite(900,250,20,300);
+  exit = createSprite(900,250,0,300);
 
 
   //make it rigid
@@ -41,14 +44,14 @@ function setup() {
   exit.immovable = true;
   //enemy sprites group defined here to allow for collider to be used.
   eSprites = new Group();
-  
+  bullets = new Group();
   
 
 
 	//function to place all enemy sprites on canvas
 	function wave(level){
 		//loop 10 times to place sprites
-		for (let i = 0; i < 30 +(level * 2); i++) {
+		for (let i = 0; i < 10 +(level * 10); i++) {
 			//wait 300ms
 			setTimeout(function(){
 				//new enemy
@@ -72,7 +75,7 @@ function setup() {
 				waveTime--;
 			// if the wave is out of time, reset the timer and stop the function
 			if (waveTime <= 0){
-				waveTime = 30;
+				waveTime = 10;
 				return;
 			}
 		},1000);
@@ -84,12 +87,13 @@ function setup() {
 		setInterval(function(){
 			if(lives <= 0) return;
 			wave(level);
-			timer();
+			
 			level++;
-		},30000);	
+		},10000);	
 	}
 	//first time calls to set up the waves
 	timer();
+	startWaves();
 
 }
 function mouseClicked() {
@@ -108,10 +112,13 @@ function Tower(x, y) {
 		this.dmg = 5;
 		//how fast it can fire, in ms
 		this.fireRate = 500;
+		this.x = x;
+		this.y = y;
 		//actual sprite on the canvas
 		this.sprite = createSprite(x,y,75,75);
 		//image for the sprite
 		this.sprite.addImage(tImage);
+		towers.push(this);
 }
 
 //enemey constructor for the waves of enemies
@@ -123,30 +130,40 @@ function Enemy(){
 		//hp stat, how many bullets can it take before removing from canvas
 		this.hp = 10;
 		//how fast the sprite moves
-		this.speed = 7;
+		this.speed = 3;
 		//what the sprite looks like
 		this.sprite.addAnimation("running",animated);
 
 }
 
-//function to shoot bullets
-function fire(enemyPos){
-	//create new bullet sprite
+// 	a tower will fire every 500ms
+// setinterval(fire(),500);
 
-	//get position of nearest enemy
+function fire(towerPosX, towerPosY, range = 10){
 
-	//send bullet at enemy
 
+// fire will create a bullet
+// bullet = createSprite()
+	var projectile = createSprite(towerPosX,towerPosY, 10,10);
+// bullet will be sent toward [attractionPoint()] the enemy
+// bullet.attractionPoint(30,eSprites[0].enemyXpos,eSprites[0].enemyYpos);
+	projectile.addToGroup(bullets);
+	projectile.life = range;
+	projectile.attractionPoint(40,enemies[0].sprite.position.x,enemies[0].sprite.position.y);
+// the location of the enemy will be updated.
 }
+
+
+
 function youLose(){
 	var box = document.querySelector('.over');
 	box.style.display = 'inherit';
 	remove();
 }
-
+var counter = 0;
 function draw() {
 
-	//add background to the canvas
+	//add background to the canvas`
 	background(img);
 	//required to have any sprites appear
   	drawSprites();
@@ -164,10 +181,18 @@ function draw() {
   	//removes enemies from canvas and enemy group and array and deducts lives.
   	eSprites.collide(exit, function(sprite){
   		sprite.remove();
-  		//lives--;
+  		lives--;
   		enemies.shift();
 
   	})
+
+  	eSprites.collide(bullets,function(sprite1,sprite2){
+  		sprite1.remove();
+  		sprite2.remove();
+  		enemies.shift();
+  		gold +=5;
+
+  	});
 
   	
 	if (lives <= 0) {
@@ -176,6 +201,17 @@ function draw() {
 		//ends the game
 		youLose();
 	}
+
+	if(counter > 60){
+		counter = 0;
+		towers.forEach(function(tower){
+			if(enemies.length > 0)
+			fire(tower.x,tower.y);
+		})
+	}
+	counter++;
+
+
 }
 
 
